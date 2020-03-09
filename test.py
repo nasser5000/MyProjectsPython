aba0995e6b110358c01312c3fc3a363ef5291617
nassersalemfarrag <nassersalemfarrag@gmail.com>
2020-03-09 13:36:51 +0300

    add a new File and create a turtle Object

---
+++ test.py
@@ -0,0 +1,14 @@
+import turtle
+terry = turtle.Turtle()
+rainbow = ["red", "orange", "yellow", "green", "blue", "purple"]
+terry.width(5)
+terry.speed(0)
+for color in rainbow:
+    terry.color(color)
+    for side in [1,2,3,4,5]:
+        terry.forward(50)
+        terry.right(144)
+    terry.right(60)
+    terry.penup()
+    terry.forward(50)
+    terry.pendown()
